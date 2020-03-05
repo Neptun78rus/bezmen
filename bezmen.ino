12d7b40c4ac8bc1188f94c7df5c07102875f0e14
Neptun78rus <neptune44444@gmail.com>
2020-03-05 19:18:38 +0300

    add Tahometr

--- bezmen.ino
+++ bezmen.ino
@@ -1,80 +1,61 @@
-#include "HX711.h"
+#define fqPin 2 //назначаем пин датчика оборотов
+#define LOADCELL_DOUT_PIN 13// 13 – пин подключения DT
+#define LOADCELL_SCK_PIN 12// 12 – пин подключения SCK
+#include "HX711.h"//библиотека АЦП тензодатчика 
 #include <LiquidCrystal.h> // Подключаем библиотеку работы с символьными дисплеями
-LiquidCrystal lcd(2, 3, 4, 5, 6, 7); // (RS, E, D4, D5, D6, D7) подключаем выходы дисплея согласно последовательнос
-// создание экземпляра объекта
-
-// 13 – пин подключения DT
-
-// 12 – пин подключения SCK
-
-const int LOADCELL_DOUT_PIN = 13;
-const int LOADCELL_SCK_PIN = 12;
+LiquidCrystal lcd(8, 3, 4, 5, 6, 7); // (RS, E, D4, D5, D6, D7) подключаем выходы дисплея согласно последовательнос
 HX711 scale;
+float scale_calibration = -1.5456;// значение калибровочного коэффициента (делать подбор для своих весов)
+float weight_units;// переменные вес в унциях
+float weight_gr;//вес в граммах
 
-// значение калибровочного коэффициента (делать подбор для своих весов)
+const int K = 1;
 
-float scale_calibration = -1.5456;
+volatile unsigned long counter; // Количество отсчётов.
+volatile unsigned long mks;     // Время последнего отсчёта.
 
-// переменные
+unsigned long oldTime;  // Время последнего отсчёта в предыдущем вычислении
+//***********Функция для обработки прерывания.
 
-float weight_units;
-
-float weight_gr;
-
-
-
-void setup() {
-
-// Инициализируем последовательный порт
-
-lcd.begin(16, 2); // Инициализируем LCD 1602
-
-lcd.print("calibration");
-scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-delay (5000);
-
-scale.set_scale();
-
-// Сбрасываем весы на 0
-
-scale.tare();
-
-
-
+void DRIG() {
+	// Здесь код прерывания
+	mks=micros(); // Момент последнего отсчёта
+	counter++;  // Количество отсчётов
 }
-
-
-
+//*******************************************
+void setup() {
+	lcd.begin(16, 2); // Инициализируем LCD 1602
+	attachInterrupt(digitalPinToInterrupt(fqPin), DRIG, RISING);// Подключаем функцию на прерывание по появлению сигнала на ноге fqPin.
+	lcd.print("calibration");
+	scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+	delay (5000);
+	scale.set_scale();
+	scale.tare(); // Сбрасываем весы на 0
+}
 void loop() {
 	lcd.clear();
-
-// Устанавливаем калибровочный коэффициент
-
-scale.set_scale(scale_calibration);
-
-// получить данные
-
-
-weight_units = scale.get_units(), 50;
-
-// если <0 то 0
-/*
-if (weight_units < 0) {
-
-weight_units = 0.00;
-
-}
-*/
-// перевод из унций в граммы
-
-weight_gr = weight_units * 0.035274;
-
-
-
-
-
-lcd.print(weight_gr);
-
-lcd.write(" gr");
-delay(1000);
+	unsigned long rpm;
+	// Получаем данные. 
+	noInterrupts();
+	unsigned long cnt = counter;
+	counter = 0;
+	unsigned long tmr = mks;
+	interrupts();
+	// Выводим частоту в оборотах в секунду
+	if (cnt > 0) {
+		rpm = 1000000UL / ((tmr - oldTime) / cnt) / K;
+		oldTime = tmr;
+	}
+	else {
+		rpm = 0;
+	}
+	lcd.print( rpm*60 );
+	lcd.write("rpm");
+	scale.set_scale(scale_calibration);// Устанавливаем калибровочный коэффициент
+	weight_units = scale.get_units(), 50;// получить данные
+	weight_gr = weight_units * 0.035274;// перевод из унций в граммы
+	lcd.setCursor(0,1);
+	lcd.print(weight_gr);
+	lcd.write(" gr");
+	delay(1000);
 }
